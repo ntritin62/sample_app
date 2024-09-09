98b92af35dfe299108f64538dbf4455ab308c156
ntritin62 <21522677@gm.uit.edu.vn>
2024-09-09 20:20:06 +0700

    Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,26 @@
 class SessionsController < ApplicationController
+  before_action :find_user, only: :create
   def new; end
 
   def create
-    user = User.find_by(email: params.dig(:session, :email)&.downcase)
-    if user&.authenticate(params.dig(:session, :password))
-      flash[:success] = t ".successful"
-      reset_session
-      log_in user
-      redirect_to user, status: :see_other
-    else
+    unless @user&.authenticate(params.dig(:session, :password))
       flash.now[:danger] = t ".invalid"
-      render :new, status: :unprocessable_entity
+      return render :new, status: :unprocessable_entity
     end
+    params.dig(:session, :remember_me) == "1" ? remember(@user) : forget(@user)
+    reset_session
+    log_in @user
+    flash[:success] = t ".successful"
+    redirect_to @user
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
+  end
+
+  private
+  def find_user
+    @user = User.find_by(email: params.dig(:session, :email)&.downcase)
   end
 end
